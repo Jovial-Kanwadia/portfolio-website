25862a8a00677eac4290388d54f694dc79980961
Zora <zora@Zoras-Mac-mini.local>
2025-10-24 22:38:26 +0530

    Initial draft

---
+++ src/sections/About.jsx
@@ -0,0 +1,63 @@
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { AnimatedTextLines } from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const About = () => {
+  const text = `PASSIONATE ABOUT SCALABLE SYSTEMS
+    I BUILD HIGH-PERFORMANCE, AI-DRIVEN SOLUTIONS
+    FROM PROTOTYPE TO PRODUCTION.`;
+  const aboutText = `I am obsessed with building fast, high-impact applications. My experience ranges from crafting pixel-perfect, real-time UIs in React and Next.js to engineering high-performance Golang backends.
+  When I’m not shipping:
+⚡️ Mentoring 200+ junior developers in web development at IIITL's Axios Technical Society.
+🧠 Solving complex challenges and competing as a Specialist on CodeForces.
+🌍 Debating global diplomacy and international relations at Model United Nations.
+💻 Organizing and leading 10+ technical workshops and coding events for my campus community.`;
+  const imgRef = useRef(null);
+  useGSAP(() => {
+    gsap.to("#about", {
+      scale: 0.95,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "bottom 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: false,
+      },
+      ease: "power1.inOut",
+    });
+
+    gsap.set(imgRef.current, {
+      clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+    });
+    gsap.to(imgRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 2,
+      ease: "power4.out",
+      scrollTrigger: { trigger: imgRef.current },
+    });
+  });
+  return (
+    <section id="about" className="min-h-screen bg-black rounded-b-4xl">
+      <AnimatedHeaderSection
+        subTitle={"Code with purpose, Built to scale"}
+        title={"About"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
+      />
+      <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
+        <img
+          ref={imgRef}
+          src="images/man.png"
+          alt="man"
+          className="w-md rounded-3xl"
+        />
+        <AnimatedTextLines text={aboutText} className={"w-full"} />
+      </div>
+    </section>
+  );
+};
+
+export default About;
